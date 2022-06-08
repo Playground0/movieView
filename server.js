35874bdf516214cba1b1eef580d9fc1dd8625092
Playground0 <emailplayg1@gmail.com>
2022-06-08 18:43:52 +0530

    Update server file

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname+'/dist/moviewview'));
+app.use(express.static(__dirname+'/dist/movie-view'));
 
 app.get('/*',(req, res) => {
-    res.sendFile(__dirname+'/dist/movieview/index.html')
+    res.sendFile(__dirname+'/dist/movie-view/index.html')
 });
 
 app.listen(process.env.PORT || 8000);

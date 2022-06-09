ac77dec55b9840bc7f70f0dbb717e1ab3b4acfde
Playground0 <emailplayg1@gmail.com>
2022-06-10 01:18:01 +0530

    Added folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrivateComponent } from './private/private.component';
+import { PublicComponent } from './public/public.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrivateComponent,
+    PublicComponent
   ],
   imports: [
     BrowserModule,
